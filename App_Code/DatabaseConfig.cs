43735d72722ac7a6f4a729b064448e7318f5245e
vivek parmar <pvs09218@gmail.com>
2022-03-27 12:15:07 +0530

    Add project files.

---
+++ App_Code/DatabaseConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for DatabaseConfig
+/// </summary>
+namespace AddressBook
+{
+    public class DatabaseConfig
+    {
+        public DatabaseConfig()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+        public static String ConnectionString = ConfigurationManager.ConnectionStrings["AddressBookConnectionString"].ConnectionString.ToString();
+    }
+}
---
+++ App_Code/ENT/CityENT.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for CityENT
+/// </summary>
+namespace AddressBook.ENT
+{
+    public class CityENT
+    {
+        #region Constructor
+        public CityENT()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        #endregion Constructor
+
+        #region CityID
+        protected SqlInt32 _CityID;
+        public SqlInt32 CityID
+        {
+            get
+            {
+                return _CityID;
+            }
+            set
+            {
+                _CityID = value;
+            }
+        }
+        #endregion CityID
+
+        #region StateID
+
+        protected SqlInt32 _StateID;
+
+        public SqlInt32 StateId
+        {
+            get
+            {
+                return _StateID;
+            }
+            set
+            {
+                _StateID = value;
+            }
+        }
+
+        #endregion StateID
+
+        #region CityName 
+        protected SqlString _CityName;
+        
+        public SqlString CityName
+        {
+            get
+            {
+                return _CityName;
+            }
+            set
+            {
+                _CityName = value;
+            }
+        }
+        #endregion CityName
+
+        #region STDCode
+
+        protected SqlString _STDCode;
+        
+        public SqlString STDCode
+        {
+            get
+            {
+                return _STDCode;
+            }
+            set
+            {
+                _STDCode = value;
+            }
+        }
+
+
+        #endregion STDCode
+
+        #region Pincode
+
+        protected SqlString _Pincode;
+        public SqlString Pincode
+        {
+            get
+            {
+                return _Pincode;
+            }
+            set
+            {
+                _Pincode = value;
+            }
+        }
+
+        #endregion Pincode
+
+        #region CreationDate
+
+        protected SqlDateTime _CreationDate;
+        public SqlDateTime CreationDate
+        {
+            get
+            {
+                return _CreationDate;
+            }
+            set
+            {
+                _CreationDate = value;
+            }
+        }
+
+        #endregion CreationDate 
+    }
+}
